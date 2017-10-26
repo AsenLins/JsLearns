ac5a9a3732cbc74a09887bd3af0d68c1647a65ad
CHZA林根森-Kim <mzh1708587@centaline.com.cn>
2017-10-26 18:37:00 +0800

    修改企业相册

---
+++ Project/微信企业相册/dataTest.js
@@ -0,0 +1,194 @@
+{
+    "indexData": [
+        {
+            "Id": "ece7a7f6-35e4-4e58-aa23-ff7dbb4756fe",
+            "Title": "新增1",
+            "Memo": "新增1",
+            "CreateAt": "2017-10-26T17:26:22.273",
+            "ImgPath": "http://hq.centanet.com/CentalineHQImage/AlbumImages/f2f15a71-26eb-4669-9f48-adba432d362f.jpg",
+            "SmallPicList": [
+                {
+                    "pic": "http://hq.centanet.com/CentalineHQImage/AlbumImages/8776361a-ef7a-45b7-bfca-f5b207d9f73b.jpg"
+                },
+                {
+                    "pic": "http://hq.centanet.com/CentalineHQImage/AlbumImages/f2f15a71-26eb-4669-9f48-adba432d362f.jpg"
+                }
+            ]
+        },
+        {
+            "Id": "492bff8b-7340-4cc0-917c-3b05fb86faa1",
+            "Title": "测试",
+            "Memo": "测试",
+            "CreateAt": "2017-10-26T17:25:24.883",
+            "ImgPath": "http://hq.centanet.com/CentalineHQImage/AlbumImages/9b631e4b-6464-461c-9e9c-ccc30a6dfb47.jpg",
+            "SmallPicList": [
+                {
+                    "pic": "http://hq.centanet.com/CentalineHQImage/AlbumImages/9b631e4b-6464-461c-9e9c-ccc30a6dfb47.jpg"
+                },
+                {
+                    "pic": "http://hq.centanet.com/CentalineHQImage/AlbumImages/a846530a-20d5-4ac0-8aaa-7cd12864d889.jpg"
+                }
+            ]
+        }
+    ],
+    "albumData": [
+        {
+            "Id": "2bbfb176-a81d-461d-a4ce-d4183b67ef03",
+            "Path": "http://hq.centanet.com/CentalineHQImage/AlbumImages/2bbfb176-a81d-461d-a4ce-d4183b67ef03.jpg",
+            "LikeCount": 0,
+            "ReviewCount": 0
+        },
+        {
+            "Id": "6ed25944-e43d-49bc-be84-5fd0672fa7a5",
+            "Path": "http://hq.centanet.com/CentalineHQImage/AlbumImages/6ed25944-e43d-49bc-be84-5fd0672fa7a5.jpg",
+            "LikeCount": 0,
+            "ReviewCount": 0
+        },
+        {
+            "Id": "2f5998b2-8313-4833-bc81-0ebee6fda65b",
+            "Path": "http://hq.centanet.com/CentalineHQImage/AlbumImages/2f5998b2-8313-4833-bc81-0ebee6fda65b.jpg",
+            "LikeCount": 0,
+            "ReviewCount": 0
+        },
+        {
+            "Id": "593e300d-3d31-4045-9380-91187bd7f72f",
+            "Path": "http://hq.centanet.com/CentalineHQImage/AlbumImages/593e300d-3d31-4045-9380-91187bd7f72f.jpg",
+            "LikeCount": 0,
+            "ReviewCount": 0
+        }
+    ],
+    "albumOneData": {
+        "ID": "66171dec-e92b-4953-ab4a-4687b00bda6d",
+        "Title": "哇哈哈",
+        "Memo": "哇哈哈",
+        "ImgPath": "http://hq.centanet.com/CentalineHQImage/AlbumImages/6ed25944-e43d-49bc-be84-5fd0672fa7a5.jpg",
+        "IsRelease": true,
+        "ReleaseCount": 10,
+        "IsDel": false,
+        "Account": null,
+        "CreateAt": "2017-10-26T15:39:40.017",
+        "PhotoLine": null,
+        "IsRemove": false,
+        "IsAdd": false,
+        "IsUpdate": false
+    },
+    "photoDetails": {
+        "PhotoList": {
+            "currentIndex": 4,
+            "list": [
+                {
+                    "src": "http://hq.centanet.com/CentalineHQImage/AlbumImages/2bbfb176-a81d-461d-a4ce-d4183b67ef03.jpg",
+                    "id": "2bbfb176-a81d-461d-a4ce-d4183b67ef03"
+                },
+                {
+                    "src": "http://hq.centanet.com/CentalineHQImage/AlbumImages/6ed25944-e43d-49bc-be84-5fd0672fa7a5.jpg",
+                    "id": "6ed25944-e43d-49bc-be84-5fd0672fa7a5"
+                },
+                {
+                    "src": "http://hq.centanet.com/CentalineHQImage/AlbumImages/2f5998b2-8313-4833-bc81-0ebee6fda65b.jpg",
+                    "id": "2f5998b2-8313-4833-bc81-0ebee6fda65b"
+                },
+                {
+                    "src": "http://hq.centanet.com/CentalineHQImage/AlbumImages/593e300d-3d31-4045-9380-91187bd7f72f.jpg",
+                    "id": "593e300d-3d31-4045-9380-91187bd7f72f"
+                },
+                {
+                    "src": "http://hq.centanet.com/CentalineHQImage/AlbumImages/1219c790-9596-4042-9502-48868c286d2d.jpg",
+                    "id": "1219c790-9596-4042-9502-48868c286d2d"
+                }
+            ]
+        },
+        "Reivew": {
+            "list": [
+                {
+                    "Content": "这是五条评论",
+                    "CreateTime": "2017-10-26T16:09:13.397",
+                    "Id": "1bcac9c0-cf22-44a1-aa5c-3cb0120e9fb5",
+                    "user": {
+                        "avatar": "http://shp.qpic.cn/bizmp/j9Y9icaeLJ2OYbdRNTjeZgyLqtFwVFOUCRQmM1wnIK70bGrtiakKpBMQ/",
+                        "name": "Stanley Poon"
+                    }
+                },
+                {
+                    "Content": "这是四条评论",
+                    "CreateTime": "2017-10-26T16:09:13.397",
+                    "Id": "1bcac9c0-cf22-44a1-aa5c-3cb0120e9fb4",
+                    "user": {
+                        "avatar": "http://shp.qpic.cn/bizmp/j9Y9icaeLJ2OYbdRNTjeZgyLqtFwVFOUCRQmM1wnIK70bGrtiakKpBMQ/",
+                        "name": "Stanley Poon"
+                    }
+                },
+                {
+                    "Content": "这是三条评论",
+                    "CreateTime": "2017-10-26T16:09:13.397",
+                    "Id": "1bcac9c0-cf22-44a1-aa5c-3cb0120e9fb3",
+                    "user": {
+                        "avatar": "http://shp.qpic.cn/bizmp/j9Y9icaeLJ2OYbdRNTjeZgyLqtFwVFOUCRQmM1wnIK70bGrtiakKpBMQ/",
+                        "name": "Stanley Poon"
+                    }
+                },
+                {
+                    "Content": "这是二条评论",
+                    "CreateTime": "2017-10-26T16:09:13.397",
+                    "Id": "1bcac9c0-cf22-44a1-aa5c-3cb0120e9fb1",
+                    "user": {
+                        "avatar": "http://shp.qpic.cn/bizmp/j9Y9icaeLJ2OYbdRNTjeZgyLqtFwVFOUCRQmM1wnIK70bGrtiakKpBMQ/",
+                        "name": "Stanley Poon"
+                    }
+                },
+                {
+                    "Content": "这是一条评论",
+                    "CreateTime": "2017-10-26T16:09:13.397",
+                    "Id": "1bcac9c0-cf22-44a1-aa5c-3cb0120e9fb0",
+                    "user": {
+                        "avatar": "http://shp.qpic.cn/bizmp/j9Y9icaeLJ2OYbdRNTjeZgyLqtFwVFOUCRQmM1wnIK70bGrtiakKpBMQ/",
+                        "name": "Stanley Poon"
+                    }
+                },
+                {
+                    "Content": "这是六条评论",
+                    "CreateTime": "2017-10-26T16:09:13.397",
+                    "Id": "1bcac9c0-cf22-44a1-aa5c-3cb0120e7fb5",
+                    "user": {
+                        "avatar": "http://shp.qpic.cn/bizmp/j9Y9icaeLJ2OYbdRNTjeZgyLqtFwVFOUCRQmM1wnIK70bGrtiakKpBMQ/",
+                        "name": "Stanley Poon"
+                    }
+                }
+            ],
+            "count": 6
+        },
+        "Like": {
+            "list": [
+                {
+                    "CreateTime": "2017-10-26T16:10:26.52",
+                    "user": {
+                        "avatar": "http://shp.qpic.cn/bizmp/j9Y9icaeLJ2OYbdRNTjeZgyLqtFwVFOUCRQmM1wnIK70bGrtiakKpBMQ/",
+                        "name": "Stanley Poon"
+                    }
+                },
+                {
+                    "CreateTime": "2017-10-26T16:10:26.52",
+                    "user": {
+                        "avatar": "http://shp.qpic.cn/bizmp/j9Y9icaeLJ2OYbdRNTjeZgyLqtFwVFOUCRQmM1wnIK70bGrtiakKpBMQ/",
+                        "name": "Stanley Poon"
+                    }
+                },
+                {
+                    "CreateTime": "2017-10-26T16:10:26.52",
+                    "user": {
+                        "avatar": "http://shp.qpic.cn/bizmp/j9Y9icaeLJ2OYbdRNTjeZgyLqtFwVFOUCRQmM1wnIK70bGrtiakKpBMQ/",
+                        "name": "Stanley Poon"
+                    }
+                },
+                {
+                    "CreateTime": "2017-10-26T16:10:26.52",
+                    "user": {
+                        "avatar": "http://shp.qpic.cn/bizmp/j9Y9icaeLJ2OYbdRNTjeZgyLqtFwVFOUCRQmM1wnIK70bGrtiakKpBMQ/",
+                        "name": "Stanley Poon"
+                    }
+                }
+            ],
+            "count": 4
+        }
+    }
+}
