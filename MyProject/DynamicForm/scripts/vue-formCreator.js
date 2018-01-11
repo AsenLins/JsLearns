47bb17bbe991785e5f6fcd7bb983acf719c27016
CHZA林根森-Kim <mzh1708587@centaline.com.cn>
2018-01-11 18:20:50 +0800

    update DynamicForm

--- MyProject/DynamicForm/scripts/vue-formCreator.js
+++ MyProject/DynamicForm/scripts/vue-formCreator.js
@@ -4,8 +4,7 @@
   支持：面板，以及表单属性
 
   2.拖拽面板
-
-
+  需要一个配置列表。
 
   根据拖拽工具菜单拖拽的内容生成对应的表单控件，表单控件可再次被拖拽
 
@@ -14,9 +13,35 @@
 
   4.生成表单
 
-  5.解析对应的动态表单
+  5.解析对应的动态表单，可生成vue表单。
+
+
+  生成完模板之后，再交给vue进行渲染。
+  需要三个vue组件：
+
+  1.form-createtor(表单生成)
+  2.form-options(表单配置)
+  3.form-reader（表单读取）
+
 
 */
+
+var InputMap={
+  "name":"myInput",
+  "type":"text",
+  "defaultVal":"cool",
+  "desc":"个人管理",
+  "placeholder":"",
+  "verify":[],
+  "events":[],
+  "link":[],
+  "wrap":""
+}
+
+var InputObj={
+  dom:null,
+
+}
 
 
 
