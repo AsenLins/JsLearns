ff90cc6317c5b7ddee6bb734b14a37729efa7c29
CHZA林根森-Kim <mzh1708587@centaline.com.cn>
2018-02-13 16:50:59 +0800

    update

---
+++ MyProject/DynamicForm2/scripts/map/formDragMap.js
@@ -0,0 +1,72 @@
+define(function(){
+
+  var formDragMap={
+    "panel":{
+      title:"",
+      icon:"",
+      imgUrl:"",
+      control:{
+        "col":{
+          icon:"",
+          imgUrl:"",
+          title:"列面板"
+        },
+        "row":{
+          icon:"",
+          imgUrl:"",
+          title:"行面板"
+        }
+      }
+    },
+    "formControl":{
+      title:"",
+      icon:"",
+      imgUrl:"",
+      control:{
+        "label":{
+          icon:"",
+          imgUrl:"",
+          title:"描述"
+        },
+        "input":{
+          icon:"",
+          imgUrl:"",
+          title:"文本框"
+        },
+        "radio":{
+          icon:"",
+          imgUrl:"",
+          title:"单选框"
+        },
+        "checkbox":{
+          icon:"",
+          imgUrl:"",
+          title:"多选框"
+        },
+        "timepicker":{
+          icon:"",
+          imgUrl:"",
+          title:"时间选择"
+        },
+        "textarea":{
+          icon:"",
+          imgUrl:"",
+          title:"多行文本框"
+        },
+        "imgUpload":{
+          icon:"",
+          imgUrl:"",
+          title:"图片上传"
+        },
+        "fileUpload":{
+          icon:"",
+          imgUrl:"",
+          title:"文件上传"
+        }
+      }
+    }
+  }
+
+  return formDragMap;
+
+});
